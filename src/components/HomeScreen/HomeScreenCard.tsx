7c75a6b8bfdff93e1b0a9915222e2d4cae295ffd
Olsen7351 <olsen7351@gmail.com>
2025-04-30 09:58:52 +0200

    navigate

--- src/components/HomeScreen/HomeScreenCard.tsx
+++ src/components/HomeScreen/HomeScreenCard.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface HomeScreenCardProps {
   icon: ReactElement<{ className: string; "aria-hidden": boolean }>;
@@ -10,24 +11,31 @@ interface HomeScreenCardProps {
   imageUrl?: string;
 }
 
-const HomeScreenCard: FC<HomeScreenCardProps> = ({icon, title, description, link, disabled}) => {
+const HomeScreenCard: FC<HomeScreenCardProps> = ({
+  icon,
+  title,
+  description,
+  link,
+  disabled,
+}) => {
+  const navigate = useNavigate();
   return (
     <div
-            className={`rounded-xl border border-gray-800 p-8 shadow-xl transition ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:border-cyan-500/10 hover:shadow-cyan-500/10'} h-60 w-60`}
+      className={`rounded-xl border border-gray-800 p-8 shadow-xl transition ${
+        disabled
+          ? "opacity-50 cursor-not-allowed"
+          : "hover:border-cyan-500/10 hover:shadow-cyan-500/10"
+      } h-60 w-60`}
     >
       <a
-                className={`${disabled ? 'pointer-events-none' : ''}`}
-        href={disabled ? undefined : link}
+        className={`${disabled ? "pointer-events-none" : ""}`}
+        onClick={() => navigate(`${link}`)}
       >
-                <div className="text-cyan-500">
-                    {icon}
-                </div>
+        <div className="text-cyan-500">{icon}</div>
 
         <h2 className="mt-4 text-xl font-bold">{title}</h2>
 
-        <p className="mt-4 text-sm text-gray-300 italic">
-          {description}
-        </p>
+        <p className="mt-4 text-sm text-gray-300 italic">{description}</p>
       </a>
     </div>
   );
