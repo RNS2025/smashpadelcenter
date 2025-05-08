10efe0cd04ef70e8e0446bc59b761f3d6036df28
Olsen7351 <119688866+Olsen7351@users.noreply.github.com>
2025-05-08 08:54:14 +0200

    Update LoginPage.tsx

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -223,12 +223,6 @@ export const LoginPage = () => {
                     </div>
                   </div>
                 </form>
-
-                <div className="flex items-center">
-                  <div className="flex-grow border-t border-gray-300"></div>
-                  <span className="mx-4 text-sm text-gray-500">eller</span>
-                  <div className="flex-grow border-t border-gray-300"></div>
-                </div>
               </div>
             </div>
           </main>
